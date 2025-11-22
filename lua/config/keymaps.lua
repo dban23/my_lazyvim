e21028e95b6c44dc2f8c6258225cb94af10398f0
dban23 <david.ban@comping.hr>
2025-11-22 14:06:38 +0100

    my tweaks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Pasting over selection possible in multiple places
+vim.keymap.set("x", "<leader>p", '"_dp', { noremap = true, silent = true })
---
+++ lua/plugins/disable_acmp.lua
@@ -0,0 +1,5 @@
+-- disabling autocomplete :)
+return {
+  --  {"hrsh7th/nvim-cmp", enabled=false},
+  { "blink.cmp", enabled = false },
+}
